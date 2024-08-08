2f32ea46f0715aacf28f7e994973accbe33170b7
sakshi2310 <cdmisakshi@gmail.com>
2024-08-08 21:49:18 +0530

    all changes

--- Admin/View_property_detailed.php
+++ Admin/View_property_detailed.php
@@ -1,3 +1,17 @@
+<?php 
+
+$con = mysqli_connect("localhost", "root", "", "real_estate");
+if(isset($_GET['view_id']))
+{
+  $view_id = $_GET['view_id'];
+  $sql = "select * from property_register where id = ".$view_id;
+  $res = mysqli_query($con, $sql);
+  $row = mysqli_fetch_assoc($res);
+  
+}
+
+
+?>
 
 <div class="wrapper">
       <!-- Sidebar -->
@@ -47,25 +61,134 @@
                     <div class="table-responsive">
                       <table
                         id="basic-datatables"
-                        class="display table table-striped table-hover"
-                      >
-                        <thead>
-                            <tr>
-                            <th>Field</th>
-                            <th>Details</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>ID</td>
-                                <td>1</td>
-                            </tr>
-                            <tr>
-                                <td>Name</td>
-                                <td>2</td>
-                            </tr>
-                        </tbody>
+                        class="display table table-striped table-hover">
+
+                        <tr>
+              <th>Field</th>
+              <th>Details</th>
+            </tr>
+            <tr>
+              <th>ID</th>
+              <td colspan="3"><?php echo $row['id']; ?></td>
+            </tr>
+            <tr>
+              <th>Property Title</th>
+              <td colspan="3"><?php echo $row['Property_title']; ?></td>
+            </tr>
+            <tr>
+              <th>Description</th>
+              <td colspan="3"><?php echo $row['Description']; ?></td>
+            </tr>
+            <tr>
+              <th>Type</th>
+              <td colspan="3"><?php echo $row['Type']; ?></td>
+            </tr>
+            <tr>
+              <th>Status</th>
+              <td colspan="3"><?php echo $row['Status']; ?></td>
+            </tr>
+            <tr>
+              <th>Sale the Rent</th>
+              <td colspan="3"><?php echo $row['Price']; ?></td>
+            </tr>
+            <tr>
+              <th>Secount Price</th>
+              <td colspan="3"><?php echo $row['Sec_price']; ?></td>
+            </tr>
+            <tr>
+              <th>Land Price</th>
+              <td colspan="3"><?php echo $row['Land_price']; ?></td>
+            </tr>
+            <tr>
+              <th>Land post fix</th>
+              <td colspan="3"><?php echo $row['Land_postfix']; ?></td>
+            </tr>
+            <tr>
+              <th style="width: 190px; height: 180px;" >Image</th>
+              <td style="width:180px; height: 180px;">
+                <img src="upload/<?php echo $row['Image1']; ?>" alt="" class="img" style="height:100%; width:100%; object-fit:cover;">
+              </td>
+              <td style="width:180px; height: 180px;">
+                <img src="upload/<?php echo $row['Image2']; ?>" alt="" class="img" style="height:100%; width:100%; object-fit:cover;">
+              </td>
+              <td style="width:180px; height: 180px;">
+                <img src="upload/<?php echo $row['Image3']; ?>" alt="" class="img" style="height:100%; width:100%; object-fit:cover;">
+              </td>
+            </tr>
+            <tr>
+              <th style="width: 190px; height: 180px;" >Image</th>
+              <td style="width:180px; height: 180px;">
+                <img src="upload/<?php echo $row['Image4']; ?>" alt="" class="img" style="height:100%; width:100%; object-fit:cover;">
+              </td>
+              <td style="width:180px; height: 180px;">
+                <img src="upload/<?php echo $row['Image5']; ?>" alt="" class="img" style="height:100%; width:100%; object-fit:cover;">
+              </td>
+              <td style="width:180px; height: 180px;">
+                <img src="upload/<?php echo $row['Image6']; ?>" alt="" class="img" style="height:100%; width:100%; object-fit:cover;">
+              </td>
+            </tr>
+            <tr>
+              <th>Features</th>
+              <td colspan="3"><?php echo $row['Feature']; ?></td>
+            </tr>
+            <tr>
+              <th>Address</th>
+              <td colspan="3"><?php echo $row['Address']; ?></td>
+            </tr>
+            <tr>
+              <th>Area Name</th>
+              <td colspan="3"><?php echo $row['Area_name']; ?></td>
+            </tr>
+            <tr>
+              <th>Area Type</th>
+              <td colspan="3"><?php echo $row['Area_type']; ?></td>
+            </tr>
+            <tr>
+              <th>City</th>
+              <td colspan="3"><?php echo $row['City']; ?></td>
+            </tr>
+            <tr>
+              <th>Badrooms</th>
+              <td colspan="3"><?php echo $row['Badrooms']; ?></td>
+            </tr>
+            <tr>
+              <th>Bathrooms</th>
+              <td colspan="3"><?php echo $row['Bathroom']; ?></td>
+            </tr>
+
+            <tr>
+              <th>Balcony</th>
+              <td colspan="3"><?php echo $row['Balcony']; ?></td>
+            </tr>
+
+            <tr>
+              <th>BHK / Plot</th>
+              <td colspan="3"><?php echo $row['BHK_plot']; ?></td>
+            </tr>
+
+            <tr>
+              <th>Total Floors</th>
+              <td colspan="3"><?php echo $row['Total_floors']; ?></td>
+            </tr>
+
+            <tr>
+              <th>Your Floors</th>
+              <td colspan="3"><?php echo $row['Your_floors']; ?></td>
+            </tr>
+
+            <tr>
+              <th>Property Age</th>
+              <td colspan="3"><?php echo $row['Property_age']; ?></td>
+            </tr>
+
+            <tr>
+              <th>Approval status</th>
+              <td colspan="3"><?php echo $row['Approval_status']; ?></td>
+            </tr>
+
+                        
                       </table>
+                     
                     </div>
                   </div>
                 </div>
