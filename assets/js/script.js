5f430e1e537565568348b218fb28c5f9fb24b23a
Vrushti Bhanderi <159320748+vrushtibhanderi02@users.noreply.github.com>
2024-09-13 00:19:25 +0530

    changes in website (13-09-24)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
  // pre-loader
- $(document).ready(function(){
-  setTimeout(function(){
-    $('.pre-loader').fadeOut();
-  },3000);
+//  $(document).ready(function(){
+//   setTimeout(function(){
+//     $('.pre-loader').fadeOut();
+//   },3000);
+// });
+
+   // Pre-loader functionality
+   $(document).ready(function() {
+    setTimeout(function() {
+        $('.pre-loader').fadeOut('slow'); // Added 'slow' to smooth the fade out transition
+    }, 3000); // 3-second delay before hiding the preloader
 });
+
